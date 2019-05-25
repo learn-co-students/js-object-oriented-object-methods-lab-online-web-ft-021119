30a48583a05af80e3084e291ed8e4cc1c0c05c14
bebopvinh <vinhdo07@gmail.com>
2019-05-25 01:58:26 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function BoardMember(name, homeState, training) {
+   this.name = name
+   this.homeState = homeState
+   this.training = training
+
+   this.veto = () => "No, I must disagree"
+
+   this.approve = function () {
+      return "You can do that!"
+   }
+
+   this.doCharity = function () {
+      return "I like to help people."
+   }
+
+   this.releasePressStatement = () => "You will see great things from Scuber."
+
+   this.sayHi = () => `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
