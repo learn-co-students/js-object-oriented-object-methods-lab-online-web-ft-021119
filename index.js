4b6e2563ffafb7184616a88df9763d44fab7d9aa
Lizzie <lchan217@gmail.com>
2019-05-25 15:55:38 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function BoardMember (name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+  this.veto = function(){
+    return "No, I must disagree"
+  }
+
+  this.approve = function(){
+    return "You can do that!"
+  }
+
+  this.doCharity = () => "I like to help people."
+  this.releasePressStatement = () => "You will see great things from Scuber."
+  this.sayHi = () => `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${training}.`
+}
